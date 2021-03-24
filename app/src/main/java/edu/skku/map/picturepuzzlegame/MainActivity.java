bb32406ee6217f8b294593c395a5af85fe525be4
seouuuukjin <uukjin4@gmail.com>
2021-03-24 23:54:25 +0900

    사진띄우기 성공 이제 그리드 뷰 삽입해야함 그리고 비트맵으로 이미지 쪼개기도 해야함

--- app/src/main/java/edu/skku/map/picturepuzzlegame/MainActivity.java
+++ app/src/main/java/edu/skku/map/picturepuzzlegame/MainActivity.java
@@ -2,7 +2,11 @@ package edu.skku.map.picturepuzzlegame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView sampleImage  = findViewById(R.id.sampleImage);
+
+        //res폴더에 저장된 사진을 Bitmap으로 만들 때 사용한다.
+        Bitmap _bit = BitmapFactory.decodeResource(getResources(), R.drawable.bear);
+
+        //Bitmap을 ImageView의 Background로 저장하기
+        BitmapDrawable bitDraw = new BitmapDrawable(getResources(), _bit);
+        sampleImage.setBackground(bitDraw);
     }
 }

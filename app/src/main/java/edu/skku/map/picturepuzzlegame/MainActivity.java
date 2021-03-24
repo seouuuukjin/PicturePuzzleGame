9f8383b58e0a054ff3b355aa638fb6a2d994923b
seouuuukjin <uukjin4@gmail.com>
2021-03-25 00:59:24 +0900

    사진이 뜨긴 하는데 근데 화면 전체의 세로 반을 가려버림 뭔가 잘못된거같음. 그리고 사진을 내가 9칸 16칸 다 잘라야 하는건가 ?

--- app/src/main/java/edu/skku/map/picturepuzzlegame/MainActivity.java
+++ app/src/main/java/edu/skku/map/picturepuzzlegame/MainActivity.java
@@ -20,41 +20,48 @@ import android.widget.ImageView;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    //밑의 정의한 클래스에서 사용하기 위해 전역변수로 설정.
 
+    int[] itemArr = new int[]{
+            R.drawable.bear
+    };
+
+    //밑의 정의한 클래스에서 사용하기 위해 전역변수로 설정.
     Activity activity = this; // == Main Activity
     GridView gridView;
-    private List<ResolveInfo> apps; // 앱정보를 기록할 List
-    private PackageManager pm; //앱을 제어할 변수를 생성
+    //private List<ResolveInfo> apps; // 앱정보를 기록할 List
+    //private PackageManager pm; //앱을 제어할 변수를 생성
 
     public class gridAdapter extends BaseAdapter{
         LayoutInflater inflater;
+        Context c;
+        int items[];
 
-        public  gridAdapter(){
-            inflater = (LayoutInflater) activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        public  gridAdapter(Context c, int arr[]){
+            this.c = c;
+            items = arr;
         };
         public final int getCount(){
             //출력할 목록 수 반환
-            return apps.size();
+            //return apps.size();
+            return items.length;
         }
         public final Object getItem(int index){
             //apps 배열에서 아이템 호출 해줌
-            return apps.get(index);
+            return null;
         }
         public final long getItemId(int index){
             //apps 배열 인덱스 구하기 위한 메소드
-            return index;
+            return 0;
         }
         //grid에 앱 정보와 새로 따로 정의한 아이템 레이아웃을 매핑 하는 메소
         public View getView(int index, View targetView, ViewGroup parent) {
 
             if(targetView == null){
+                LayoutInflater inflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 targetView = inflater.inflate(R.layout.item, parent, false);
             }
-            //앱 목록에서 지금 이 메소드에 mapping 할 한개의 app 정보만 얻어온다.
-            final ResolveInfo info = apps.get(index);
             ImageView imageView = (ImageView)targetView.findViewById(R.id.imagePuzzle);
-            imageView.setImageDrawable(info.activityInfo.loadIcon(getPackageManager()));
+            imageView.setImageResource(items[index]);
 
             return targetView;
         }
@@ -65,6 +72,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ImageView sampleImage  = findViewById(R.id.sampleImage);
+        gridView = findViewById(R.id.gridView);
+//        pm = getPackageManager();
+//        apps = pm.queryIntentActivities(main)
 
         //res폴더에 저장된 사진을 Bitmap으로 만들 때 사용한다.
         Bitmap _bit = BitmapFactory.decodeResource(getResources(), R.drawable.bear);
@@ -72,5 +82,8 @@ public class MainActivity extends AppCompatActivity {
         //Bitmap을 ImageView의 Background로 저장하기
         BitmapDrawable bitDraw = new BitmapDrawable(getResources(), _bit);
         sampleImage.setBackground(bitDraw);
+
+        gridAdapter customAdapter = new gridAdapter(this, itemArr);
+        gridView.setAdapter(customAdapter);
     }
 }
